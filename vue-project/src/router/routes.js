d6b7d3cedc0ec3e359ec5adc8390a41135fc47b1
JDEast1029 <852778359@qq.com>
2017-10-25 16:20:14 +0800

    懒加载，打包压缩

--- vue-project/src/router/routes.js
+++ vue-project/src/router/routes.js
@@ -1,15 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+//懒加载
+const HelloWorld = () => import(/* webpackChunkName: "hello" */ '@/components/HelloWorld');
+const Home = () => import(/* webpackChunkName: "home" */ '@/containers/Home/App');
+const Stepper = () => import(/* webpackChunkName: "stepper" */ '@/containers/Stepper/App');
 
 Vue.use(Router);
 
 export default new Router({
+	mode: 'history',                    //HTML5 history模式
 	routes: [
 		{
 			path: '/',
-			name: 'Hello',
+			name: 'Home',
+			component: Home
+		},
+		{
+			path: '/hello',
+			name: 'HelloWorld',
 			component: HelloWorld
+		},
+		{
+			path: '/stepper',
+			name: 'Stepper',
+			component: Stepper
 		}
 	]
 });
